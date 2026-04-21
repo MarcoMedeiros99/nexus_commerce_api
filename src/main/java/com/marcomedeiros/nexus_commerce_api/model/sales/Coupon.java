1db8d520a9343e64436b0b725c3e6d46c1efd38d
MarcoMedeiros99 <marcomedeiros.devsistemas@gmail.com>
2026-04-21 20:11:16 -0300

    feat(sales): implement Coupon entity with validation and expiry control
    - Created Coupon entity for discount management.
    
    - Added nameCode with unique constraint and LocalDateTime for expiry.
    
    - Configured discountPercentage and active status with Builder defaults.

--- src/main/java/com/marcomedeiros/nexus_commerce_api/model/sales/Coupon.java
+++ src/main/java/com/marcomedeiros/nexus_commerce_api/model/sales/Coupon.java
@@ -1,4 +1,40 @@
 package com.marcomedeiros.nexus_commerce_api.model.sales;
 
-public class Coupon {
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.xml.crypto.Data;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_coupon")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Coupon implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCoupon;
+
+    @Column(nullable = false, unique = true, length = 15)
+    private String nameCode;
+
+    @Column(nullable = false)
+    private Integer discountPercentage;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean active = true;
 }

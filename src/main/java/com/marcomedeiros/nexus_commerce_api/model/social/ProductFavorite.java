892696c43e330cc373702842f4c6a8932c304f33
MarcoMedeiros99 <marcomedeiros.devsistemas@gmail.com>
2026-04-21 22:47:29 -0300

    feat(social): implement ProductFavorite entity

---
+++ src/main/java/com/marcomedeiros/nexus_commerce_api/model/social/ProductFavorite.java
@@ -0,0 +1,41 @@
+package com.marcomedeiros.nexus_commerce_api.model.social;
+
+import com.marcomedeiros.nexus_commerce_api.model.access.User;
+import com.marcomedeiros.nexus_commerce_api.model.catalog.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_product_favorite")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ProductFavorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idFavorite;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_user", nullable = false)
+    @EqualsAndHashCode.Include
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_product", nullable = false)
+    @EqualsAndHashCode.Include
+    private Product product;
+
+    @Column(nullable = false)
+    private LocalDateTime addFavorite;
+
+    @PrePersist
+    protected void onCreate() {
+        this.addFavorite = LocalDateTime.now();
+    }
+}

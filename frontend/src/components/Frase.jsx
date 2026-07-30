3afc111412cb402cc9c71811bdaad4a39affc8d0
MarcoMedeiros99 <marcomedeiros.devsistemas@gmail.com>
2026-07-29 21:09:29 -0300

    chore: restructure project with frontend and backend directories

---
+++ frontend/src/components/Frase.jsx
@@ -0,0 +1,9 @@
+function Frase() {
+    return (
+        <div>
+            <p>esse é um componete com uma frase</p>
+        </div>
+    )
+};
+
+export default Frase;
---
+++ frontend/src/components/HelloWord.jsx
@@ -0,0 +1,14 @@
+import Frase from "./Frase";
+
+function HelloWord() {
+    return (
+        <div>
+            <h1>meu primeiro codigo</h1>
+            <Frase />
+        </div>
+    )
+
+
+};
+
+export default HelloWord;
